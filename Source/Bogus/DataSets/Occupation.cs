404dd25b1d46c303d2362abcb51387bfe90d45f7
vasko <sp0x@xakep.ru>
2017-04-23 12:50:40 +0300

    Added occupations

---
+++ Source/Bogus/DataSets/Occupation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bogus.DataSets
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Occupation : DataSet
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="locale"></param>
+        public Occupation(string locale = "en") : base(locale)
+        {
+        }
+
+        /// <summary>
+        /// Get a random lorem word.
+        /// </summary>
+        public string Get()
+        {
+            return this.GetRandomArrayItem("items");
+        }
+
+        /// <summary>
+        /// Get some lorem words
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public string[] Occupations(int num = 3)
+        {
+            return Enumerable.Range(1, num).Select(f => Get()).ToArray(); // lol
+        }
+         
+        /// <summary>
+        /// Slugify lorem words.
+        /// </summary>
+        /// <param name="wordcount"></param>
+        public string Slug(int wordcount = 3)
+        {
+            var words = Occupations(wordcount);
+            return Utils.Slugify(string.Join(" ", words));
+        } 
+
+    }
+}
